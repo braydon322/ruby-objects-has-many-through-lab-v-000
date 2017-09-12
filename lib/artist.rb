7d2e0c8c3de6f2c760d728e213787e8eff7226b4
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-12 06:07:20 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,11 +5,13 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
+    @genres = []
   end
 
   def add_song(song)
     @songs << song
     song.artist = self
+    @genres << song.genre
   end
 
 
