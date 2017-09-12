129e7e36140b03d444a0351e71d21be243af59af
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-12 06:00:17 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,14 @@
 class Artist
 
-  attr_accessor :name
+  attr_accessor :name, :songs, :genres
 
   def initialize(name)
     @name = name
     @songs = []
     end
+
+    def add_song
+    end
+    
 
 end
