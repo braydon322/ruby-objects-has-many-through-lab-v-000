b80e4aa54a6fc4d020ecd4e499400fcfbf3671dc
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-12 06:18:29 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
 
-  attr_accessor 
+  attr_accessor
 
   def initialize(name)
     @name = name
@@ -10,7 +10,8 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+  end
 
-  
+
 
 end
