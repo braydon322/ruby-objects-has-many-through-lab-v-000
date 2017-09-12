17006c3700ce2ed19e5b3781c9ba8d4b1f8f8697
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-12 06:58:16 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,11 +4,12 @@ class Patient
   def initialize(name)
     @name = name
     @appointments = []
+    @doctors = []
   end
 
   def add_appointment(appointment)
     @appointments << appointment
     appointment.patient = self
-
+    @doctors << appointment.doctor
   end
 end
